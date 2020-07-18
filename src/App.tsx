4d703cb3f77cc2f83df8ffcd1d13f0e225d77b97
thebat93 <ais_93@mail.ru>
2020-07-18 19:06:38 +0300

    Работа со стейтом

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  const todos = [{ id: "1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const todoAddHandler = (text: string) => {};
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
+  };
 
   return (
     <div className="App">
